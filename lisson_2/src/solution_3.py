a1ed6f689d7efd82dc2739aa87b8bbcc96b3f9cc
Egor Shpachenko <egor.shpachencko@yandex.ru>
2023-12-12 20:13:35 +0300

    Lesson 2

---
+++ lisson_2/src/solution_3.py
@@ -0,0 +1,9 @@
+#  3 
+book=int(input("Введите колличество книг:="))
+stationeru=int(input("Введите количество игрушек:="))
+tous=int(input("Введите количетсво игрушек:="))
+volume_book= book * 2
+volume_statineru= stationeru * 1.5
+volume_tous= tous * 3
+volume= volume_book + volume_statineru + volume_tous
+print("Общий обьем товаров:=", volume,  "m3")
